914eb15bf6e16fa4400432edc02f7efe9cfd0766
cmystic1 <carlillut7@gmail.com>
2026-07-04 21:26:46 +0800

    setup backend express server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
-const exp = express();
+import cors from "cors";
+const app = express();
 const PORT = 3000;
 
-exp.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-exp.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
